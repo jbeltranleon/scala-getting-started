f44fe14255608b932cc782a7779174e9e09f145f
Jhon.beltran <jhon.beltran@payulatam.com>
2021-05-29 18:30:28 -0500

    high order funcions using linear recursion

--- src/main/scala/example/HighOrderFunctions.scala
+++ src/main/scala/example/HighOrderFunctions.scala
@@ -13,6 +13,20 @@ object HighOrderFunctions {
     if (a>b) 0 else cube(a) + sumCubesInRange(a+1, b)
 
 
+  // Generating High Order functions
+  // Lets define the sum generic function this function will recive a function as a parameter!
+  // functionName: ParamType => ReturnType
+  def sum(f: Int => Int, a: Int, b: Int): Int =
+    if (a>b) 0 else f(a) + sum(f, a+1, b)
+
+  def fact(x: Int): Int = if (x==0) 1 else fact(x-1)
+
+  // The return type could be inferred by the compiler
+  def sumFactorialsInRange(a: Int, b: Int) = sum(fact, a, b)
+
+  // We will use an anonymous function
+  def sumSquaresInRange(a: Int, b: Int) = sum((x: Int) => x*x, a, b)
+
   def main(args: Array[String]): Unit = {
     val a = 2
     val b = 4
@@ -22,7 +36,12 @@ object HighOrderFunctions {
     val sumCubesResult = sumCubesInRange(a, b)
     println(s"The result of the sum cubes is: $sumCubesResult")
 
-  }
+    val sumFactorialsResult = sumFactorialsInRange(a, b)
+    println(s"The result of the sum factorials is: $sumFactorialsResult")
 
+    val sumSquaresResult = sumSquaresInRange(a, b)
+    println(s"The result of the sum squares is: $sumSquaresResult")
+
+  }
 
 }
